b979f6afbec2dd8330b27dc510c71593893848fc
Yufei Wang <1127800295@qq.com>
2026-06-17 07:13:46 +0000

    spilit model

--- cifar10/model_avialiable.py
+++ cifar10/model_avialiable.py
@@ -97,7 +97,7 @@ class SpikingSelfAttention(nn.Module):
         return x
 
 
-class SpikingTransformer1(nn.Module):
+class SpikingTransformer(nn.Module):
     def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
                  drop_path=0., norm_layer=nn.LayerNorm, sr_ratio=1):
         super().__init__()
@@ -114,56 +114,6 @@ class SpikingTransformer1(nn.Module):
         x = x + self.mlp(x)
         return x
 
-class SpikingTransformer2(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., norm_layer=nn.LayerNorm, sr_ratio=1):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = SpikingSelfAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                           attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
-
-    def forward(self, x):
-        x = x + self.attn(x)
-        x = x + self.mlp(x)
-        return x
-    
-class SpikingTransformer3(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., norm_layer=nn.LayerNorm, sr_ratio=1):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = SpikingSelfAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                           attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
-
-    def forward(self, x):
-        x = x + self.attn(x)
-        x = x + self.mlp(x)
-        return x
-    
-class SpikingTransformer4(nn.Module):
-    def __init__(self, dim, num_heads, mlp_ratio=4., qkv_bias=False, qk_scale=None, drop=0., attn_drop=0.,
-                 drop_path=0., norm_layer=nn.LayerNorm, sr_ratio=1):
-        super().__init__()
-        self.norm1 = norm_layer(dim)
-        self.attn = SpikingSelfAttention(dim, num_heads=num_heads, qkv_bias=qkv_bias, qk_scale=qk_scale,
-                                           attn_drop=attn_drop, proj_drop=drop, sr_ratio=sr_ratio)
-        self.drop_path = DropPath(drop_path) if drop_path > 0. else nn.Identity()
-        self.norm2 = norm_layer(dim)
-        mlp_hidden_dim = int(dim * mlp_ratio)
-        self.mlp = MLP(in_features=dim, hidden_features=mlp_hidden_dim, drop=drop)
-
-    def forward(self, x):
-        x = x + self.attn(x)
-        x = x + self.mlp(x)
-        return x
 
 class SpikingTokenizer(nn.Module):
     def __init__(self, img_size_h=128, img_size_w=128, patch_size=4, in_channels=2, embed_dims=256):
@@ -243,67 +193,11 @@ class vit_snn(nn.Module):
                           in_channels=in_channels,
                           embed_dims=embed_dims)
         num_patches = patch_embed.num_patches
-        # block = nn.ModuleList([SpikingTransformer(
-        #     dim=embed_dims, num_heads=num_heads, mlp_ratio=mlp_ratios, qkv_bias=qkv_bias,
-        #     qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[j],
-        #     norm_layer=norm_layer, sr_ratio=sr_ratios)
-        #     for j in range(depths)])
-
-        block = nn.ModuleList()
-        block.append(
-                SpikingTransformer1(
-                    dim=embed_dims,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratios,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[0],
-                    norm_layer=norm_layer,
-                    sr_ratio=sr_ratios
-                ))
-        block.append(
-                SpikingTransformer2(
-                    dim=embed_dims,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratios,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[1],
-                    norm_layer=norm_layer,
-                    sr_ratio=sr_ratios
-                ))
-        block.append(
-                SpikingTransformer3(
-                    dim=embed_dims,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratios,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[2],
-                    norm_layer=norm_layer,
-                    sr_ratio=sr_ratios
-                ))
-        block.append(
-                SpikingTransformer4(
-                    dim=embed_dims,
-                    num_heads=num_heads,
-                    mlp_ratio=mlp_ratios,
-                    qkv_bias=qkv_bias,
-                    qk_scale=qk_scale,
-                    drop=drop_rate,
-                    attn_drop=attn_drop_rate,
-                    drop_path=dpr[3],
-                    norm_layer=norm_layer,
-                    sr_ratio=sr_ratios
-                ))
-
-
+        block = nn.ModuleList([SpikingTransformer(
+            dim=embed_dims, num_heads=num_heads, mlp_ratio=mlp_ratios, qkv_bias=qkv_bias,
+            qk_scale=qk_scale, drop=drop_rate, attn_drop=attn_drop_rate, drop_path=dpr[j],
+            norm_layer=norm_layer, sr_ratio=sr_ratios)
+            for j in range(depths)])
 
         setattr(self, f"patch_embed", patch_embed)
         setattr(self, f"block", block)
