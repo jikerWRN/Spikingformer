e870895eb44fca44d76095a33b73fa77840a0c9d
Yufei Wang <1127800295@qq.com>
2026-06-26 08:04:46 +0000

    debug adaptive neuron

--- train_cifar10_random.sh
+++ train_cifar10_random.sh
@@ -78,6 +78,28 @@ CUDA_VISIBLE_DEVICES=1 python cifar10/train.py -c cifar10/cifar10.yml --model-fi
 a
 
 
+# ----------------------------------------------------------------
+20260626
+
+CUDA_VISIBLE_DEVICES=0 python cifar10/train_count.py -c cifar10/cifar10.yml --model-file model_4layers_baseline_ILIF01_count -data-dir /home/wangyufei/dataset --output /home/wangyufei/code/SNNTransformer/Spikingformer/output/train --experiment Spikingformer-4-384-random --iter-eval-start-epoch 350 --iter-eval-interval 100
+
+
+d
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 # ------------------------------# -------------------------------- # -------------------
 随机模型
 
